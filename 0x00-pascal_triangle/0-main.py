79108f8efebb53e16c29cba774bdfd0bc1447acf
BadrRibzat <badrribzat@gmail.com>
2024-03-06 04:07:07 -0800

    test-file

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -2,11 +2,12 @@
 """
 0-main
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+pascal_triangle = __import__ ('0-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
     """
-    Print the triangle
+    Printing the triangle
+
     """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
@@ -14,3 +15,4 @@ def print_triangle(triangle):
 
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
+
