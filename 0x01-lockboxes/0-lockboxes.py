65409fe118ae13056ae2f115fa18f47ad76ae0ee
BadrRibzat <badrribzat@gmail.com>
2024-03-12 13:24:10 -0700

    lockboxes task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/pythone3
+"""
+    Description:
+    Write a method that determines if all the boxes can be opened
+    Arguments:
+    boxes --> List of Lists, it contains the boxes with keys
+    Reurn boolean
+    Variables:
+    myKeys --> List, Store the number keys to open boxes
+    key --> integer, key of the myKeys
+    boxKey --> iinteger, key inside of an specific box
+    """
+def canUnlockAll(boxes):
+
+    myKeys = [0]
+    for key in myKeys:
+        for boxKey in boxes[key]:
+            if boxKey not in myKeys and boxKey < len(boxes):
+                myKeys.append(boxKey)
+    if len(myKeys) == len(boxes):
+        return True
+    return False
