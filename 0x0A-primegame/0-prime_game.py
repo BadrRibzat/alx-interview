0b3ff0683336a8966e3fc31b86cf93f8aff02ca3
badrRibzat <badrribzat@gmail.com>
2024-07-02 08:54:18 -0700

    updated

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,55 +1,29 @@
+#!/usr/bin/python3
+""" Prime Game """
+
+
 def isWinner(x, nums):
-    """Function that determine who has won the most rounds of the prime game."""
-    mariaWinsCount = 0
-    benWinsCount = 0
-
-    for num in nums:
-        roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
-
-        if not primesSet:
-            benWinsCount += 1
-            continue
-
-        isMariaTurn = True
-
-        while True:
-            if not primesSet:
-                if isMariaTurn:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
-            smallestPrime = primesSet.pop(0)
-            roundsSet.remove(smallestPrime)
-            roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
-            primesSet = [p for p in primesSet if p in roundsSet]
-
-            isMariaTurn = not isMariaTurn
-
-    if mariaWinsCount > benWinsCount:
-        return "Maria"
-    if benWinsCount > mariaWinsCount:
-        return "Ben"
-    return None
-
-
-def is_prime(n):
-    """Returning True if n is prime, else False."""
-    if n < 2:
-        return False
+    """ Function to determine who has won the most rounds of the prime game """
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    nums.sort()
+    m = [False for i in range(n + 1)]
     for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def primes_in_range(start, end):
-    """Returning a list of prime numbers between start and end 'inclusive'."""
-    return [n for n in range(start, end + 1) if is_prime(n)]
-
-
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
-
+        if not m[i]:
+            for j in range(i*i, n + 1, i):
+                m[j] = True
+    m[0] = m[1] = True
+    c = 0
+    for i in range(len(m)):
+        if not m[i]:
+            c += 1
+        m[i] = c
+    p1 = 0
+    for n in nums:
+        p1 += m[n] % 2 == 1
+    if p1 * 2 == len(nums):
+        return None
+    if p1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
